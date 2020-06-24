4f75e3995ee8db2dff47e6bdec94035521c52f3c
Jones Sarmento <falecomjones@gmail.com>
2020-06-24 17:37:18 -0300

    Primeiro Commit

---
+++ src/main/java/jones/io/projeto1/entity/Role.java
@@ -0,0 +1,25 @@
+package jones.io.projeto1.entity;
+import javax.persistence.*;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	private String name;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
